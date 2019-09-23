85967d486881333853d362b3c3aba1b291cf2a3f
Yessivr982218 <44659938+Yessivr982218@users.noreply.github.com>
2019-09-22 21:38:13 -0500

    Add files via upload

---
+++ Yessica Verdugo Rueda/Dora/Dora/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Dora
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+            long x,y,n,e,g;
+         
+            Console.WriteLine("Ingresa el valor de X:");
+            Console.ReadLine();
+            Console.WriteLine("\nIngresa el valor de Y:");
+            Console.ReadLine();
+
+
+
+            if (x >= 1 && x <= 2000000000)
+            {
+                e = (n + 1) * (n / 2);
+                g = (e - x) + 1;
+                if (y >= 1 && y <= 2000000000)
+                {
+                    e = (n + 1) * (n / 2);
+                    g = (e - y) + 1;
+                    Console.WriteLine("De acuerdo a las coordenadas, Dora se encuentra en: " + g);
+                    Console.ReadLine();
+                }
+
+                else
+                {
+
+                    Console.WriteLine("No puede ser aceptado");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+ }
+
+
+
---
+++ Yessica Verdugo Rueda/DoraExploradora/DoraExploradora/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DoraExploradora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime tiempo1 = DateTime.Now;
+
+            long x;
+            long y;
+            long n, e, g;
+
+
+
+
+
+            Console.WriteLine("Ingrese el valor de X: ");
+            while (!long.TryParse(Console.ReadLine(), out x))
+            {
+
+                Console.WriteLine("Valores erroneos");
+            }
+            Console.WriteLine("\nIngresa el valor de Y: ");
+            while (!long.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("Valores erroneos");
+            }
+
+
+
+            n = (x + y) - 1;
+
+            if (x > 0 && x <= 2000000000)
+            {
+                g = (n + 1) * (n / 2);
+                e = (g - x) + 1;
+
+
+                if (y > 0 && y <= 2000000000)
+                {
+
+                    g = (n + 1) * (n / 2);
+                    e = (g - y) + 1;
+                    Console.WriteLine("De acuerdo a las coordenadas, Dora se encuentra en: " + e);
+                    Console.ReadLine();
+
+                }
+
+                else
+                {
+                    Console.WriteLine("No puede ser aceptado");
+                }
+
+                DateTime tiempo2 = DateTime.Now;
+                TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+                Console.Write("TIEMPO: " + total.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
+   
+         
+ 
+    
+
